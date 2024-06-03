55455959f98e76180e1ef8c4f07ebb97232d6077
Mrityunjay Kumar <100058180+iMrityunjayKr@users.noreply.github.com>
2024-06-03 13:06:48 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"user strict";//treat all JS code as newer version
+
+//alert(3+5) //--> we are using node, not browser.
+
+// console.log(3
+//     +
+//     5)//code readability must be high
+
+// console.log("Mrityunjay")
+
+let age =15
+let state
+let loggedIN = true
+let firstName= "Mrityunjay"
+let value = null
+console.log(typeof(age))
+console.log(typeof(state))
+console.log(typeof(loggedIN))
+console.log(typeof(firstName))
+console.log(typeof(value))
+
+
