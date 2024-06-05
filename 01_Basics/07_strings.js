62b26e8f8171baa0df05deead4fd54eef3660e58
Mrityunjay Kumar <100058180+iMrityunjayKr@users.noreply.github.com>
2024-06-05 20:06:45 +0000

    strings in js

---
+++ 01_Basics/07_strings.js
@@ -0,0 +1,28 @@
+const name = "Mrityunjay"
+const repoCount = 40
+
+console.log(name + " " + repoCount + " " + "vlaue")
+
+console.log(`My name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String ("travelling-bytes")
+
+console.log(gameName.toUpperCase());
+console.log(gameName.length);
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const anotherString = gameName.slice(-15, )
+console.log(anotherString);
+
+const newStringOne = "         mrityunjay         "
+console.log(newStringOne)
+console.log(newStringOne.trimStart());
+
+const url = "https://www.youtube.com/travellin%20gbytes"
+
+console.log(url.replace('%20',''))
+
+console.log(url.includes('travel'))
+
+console.log(gameName.split('-'))
