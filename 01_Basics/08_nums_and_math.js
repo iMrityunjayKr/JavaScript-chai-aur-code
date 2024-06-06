77846fd878bc0a32589e8c3b338f52dad5b549e2
Mrityunjay Kumar <100058180+iMrityunjayKr@users.noreply.github.com>
2024-06-06 04:37:42 +0000

    nu

---
+++ 01_Basics/08_nums_and_math.js
@@ -0,0 +1,18 @@
+const score = 400
+// console.log(score)
+
+const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(5));
+const otherNumber = 23.8966
+// console.log(otherNumber.toPrecision(5));
+
+
+const hundreds = 1000000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//++++++++++++ Maths ++++++++++++++
+console.log(Math);
+console.log(Math.abs(-4));
