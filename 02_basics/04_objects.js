7fdab923c26a91fb974ff5fc920dfe5d5a54126d
Mrityunjay Kumar <100058180+iMrityunjayKr@users.noreply.github.com>
2024-06-10 14:36:26 +0000

    de structuring of objects and a bit of api

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -29,7 +29,7 @@ const obj3 = {5: "e", 6: "f"}
 
 const obj4 = {...obj1, ...obj2, ...obj3}
 
-console.log(obj4)
+// console.log(obj4)
 
 const users = [
     {
@@ -46,10 +46,31 @@ const users = [
     },
 
 ]
-console.log(users[1].email);
+// console.log(users[1].email);
 
-console.log(tinderUser)
-console.log(Object.keys(tinderUser))
-console.log( typeof Object.values(tinderUser))
-console.log(Object.entries(tinderUser))
-console.log(tinderUser.hasOwnProperty('isLoggedIN'))
+// console.log(tinderUser)
+// console.log(Object.keys(tinderUser))
+// console.log( typeof Object.values(tinderUser))
+// console.log(Object.entries(tinderUser))
+// console.log(tinderUser.hasOwnProperty('isLoggedIN'))
+
+
+const course = {
+    coursename: "Js in hindi",
+    price: "999",
+    courseInstructor: "Hitesh Choudhary Sir"
+}
+
+// course.courseInstructor // this is method to gain access over courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor) // earlier it was courseInstructor but now it has been changed to Instructor.
+console.log(instructor)
+
+
+// {
+//     "coursename": "Js in hindi",
+//     "price": "Free",
+//     "courseInstructor": "Hitesh Choudhary Sir"
+// }
