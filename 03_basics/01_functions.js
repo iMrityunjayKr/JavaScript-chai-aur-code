5b0133232f92a9df99a9f79cc5a3f80f21e9f6ef
Mrityunjay Kumar <100058180+iMrityunjayKr@users.noreply.github.com>
2024-06-11 08:32:49 +0000

    learning about rest operator, passing an object and an array in a function.

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -20,4 +20,28 @@ function loginUserMessage(username = "sam"){
 }
 // console.log(loginUserMessage("Mrityunjay")) // this will not return anything because function's return is not saved anywhere and so it is not returning anything.
 
-console.log(loginUserMessage("Mrityunjay"))
+// console.log(loginUserMessage("Mrityunjay"))
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+// console.log(calculateCartPrice(200, 400, 500,600,800)); 
+
+const user = {
+    user_name : "Mrityunjay",
+    prices : "399"
+}
+function handleObject (anyobject)
+{
+    console.log(`Username is ${anyobject.user_name} and price is ${anyobject.price}`)
+}// handleObject(user)
+handleObject({
+    user_name : "Mrityunjay",
+    price: 599
+})
+
+const myNewArray = [300, 400, 500, 600, 700, 800]
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray))
