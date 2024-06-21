32269bdf331b5a5a3593dcedc668246fb432cb49
Mrityunjay Kumar <100058180+iMrityunjayKr@users.noreply.github.com>
2024-06-21 06:57:39 +0000

    while loop, and do will loop.

---
+++ 05_iteration/02_while_do_while.js
@@ -0,0 +1,19 @@
+let index = 0
+while (index <=10) {
+    //console.log(`Value of index is ${index}`);
+    index+=2
+}
+
+let myArray = ['flash', 'batman', 'superman', 'shaktiman']
+let arr = 0;
+while(arr<myArray.length)
+    {
+        // console.log(`Value of myArray[${arr}] is : ${myArray[arr]}`);
+        arr+=1;
+    }
+let score = 1
+score = 11 
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while(score<=10)
