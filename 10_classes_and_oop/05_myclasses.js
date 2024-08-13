6eb699105fe3709ed6aefe98e0fb340ce7a7910e
Mrityunjay Kumar <100058180+iMrityunjayKr@users.noreply.github.com>
2024-08-13 01:31:08 +0000

    classes and behind the scene of classes.

---
+++ 10_classes_and_oop/05_myclasses.js
@@ -0,0 +1,39 @@
+//ES6 ke baad
+
+// class User {
+//     constructor(username, email, password) {
+//         this.username = username;
+//         this.email = email;
+//         this.password = password;
+//     }
+//     encryptPassword() {
+//         return `${this.password}abc`
+//     }
+//     changeUsername() {
+//         return `${this.username.toUpperCase()}`
+//     }
+
+// }
+// const chai = new User("chai", "chai@gmail.com", "12345")
+
+// console.log(chai.encryptPassword());
+// console.log(chai.changeUsername());
+
+//behind the scene
+
+function User(username, email, password) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+}
+User.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+}
+User.prototype.changeUsername = function() {
+    return `${this.username.toUpperCase()}`
+}
+
+const tea = new User("tea", "tea@gmail.com", "1245")
+
+console.log(tea.encryptPassword());
+console.log(tea.changeUsername());
